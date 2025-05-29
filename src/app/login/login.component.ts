6976026a3887b4b13019b4283d63040076969e56
Saurav3804 <patel0987saurav@gmail.com>
2025-05-29 10:57:44 +0530

    Add files via upload

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './login.component.html'
+})
+export class LoginComponent {
+user = {
+    email: '',
+    password: ''
+  }
+
+  submitted = false;
+
+
+  onSubmit() {
+    this.submitted = true;
+  }
+}
